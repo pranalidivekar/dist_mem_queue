5e24e070f35c776aef087da23a6d825a1caf6b08
Bryan Amador <ma5339@rit.edu>
2024-04-29 13:03:18 -0400

    corrected install

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,7 @@ CENV=`conda info --envs`
 
 if grep -w "dist_mem_queue" <<< "$CENV"
 then
-	mymsg "Conda dist_mem_queue environment already created."
+	echo "Conda dist_mem_queue environment already created."
 else
 	conda create -n dist_mem_queue -y python=3.10
 	CONDSH=`conda info | grep 'base environment' | awk '{print $4}'`/etc/profile.d/conda.sh 
